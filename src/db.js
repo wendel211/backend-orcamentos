d990b580ffac0bb579126e345cf5cd9a177f2730
wendel211 <wendelmuniz04@gmail.com>
2026-02-26 12:27:27 -0300

    fix: Error the backend logs revealed an ERR_INVALID_URL error, indicating that the DATABASE_URL format is causing issues.

--- src/db.js
+++ src/db.js
@@ -3,6 +3,9 @@ const { Pool } = pkg;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.on("connect", () => {
